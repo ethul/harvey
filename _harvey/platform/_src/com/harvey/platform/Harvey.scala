f60ecbb0ef7a71089f52eb93dff57aea1e11a7a1
eric thul <eric.thul@gmail.com>
2010-02-22 21:19:20 -0500

    initial commit

---
+++ _harvey/platform/_src/com/harvey/platform/Harvey.scala
@@ -0,0 +1,14 @@
+package com.harvey.platform
+
+/**
+ * this object is the entry point
+ * 
+ * @author ethul
+ * @date 2010.02.22
+ * @email eric.thul AT gmail.com
+ */
+object Harvey {
+  def main(args: Array[String]) {
+    println("test")
+  }
+}
